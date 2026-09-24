d6eff911fb4580b4c7c773621e5ab23d95a6955e
monai <monai@cure.lt>
2026-09-24 09:52:10 +0300

    Cleanup

--- pi/agent/extensions/stream-fallback/index.ts
+++
@@ -1,7 +0,0 @@
-import { installKeepalivePatch } from "./src/keepalive-patch.js";
-import { installVllmStallFallback } from "./src/vllm-stall-fallback.js";
-
-installKeepalivePatch();
-installVllmStallFallback();
-
-export default function () {}
--- pi/agent/extensions/stream-fallback/src/keepalive-patch.ts
+++
@@ -1,8 +0,0 @@
-import net from "node:net";
-
-export function installKeepalivePatch() {
-  const originalSetKeepAlive = net.Socket.prototype.setKeepAlive;
-  net.Socket.prototype.setKeepAlive = function (..._args: unknown[]) {
-    return originalSetKeepAlive.call(this, false);
-  };
-}
